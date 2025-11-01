ddda743bf04cbe0669b1fa1dcf4638fe383a1804
Electrodiux-pbh <github@pablobh.com>
2025-11-01 19:20:34 +0100

    Exercise IV.30

---
+++ sesion-7/IV.30_header_definition.h
@@ -0,0 +1,9 @@
+/**
+ * @author Pablo Bermejo Hernandez
+ */
+
+#include <string>
+
+void PrintCharacterLine(int lenght, char chr);
+
+void PrintHeader(const std::string& msg, char decorator = '-');
